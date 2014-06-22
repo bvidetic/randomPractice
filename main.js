356ed8d6aaebf374a68f9969fc6d794b811cc00f
brian <videticb@gmail.com>
2014-06-21 21:37:04 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+// 1. Change the background color of 'target1'.
+$(".target1").css("background-color", "blue");
+
+
+// 2. Change the text in the span, a child of 'target2'
+$(".span1").html("hello");
+
+// 3. Create a clone of the span in 'target3' and position it under the orgin.
+$(".span2").clone().insertAfter(".span2");
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+// chosen from: jqexercise.droppages.com
+
+// 4. Chage background color of the second '.target'.
+// Disabled the button.
+// Uncheck all checkbox by script
+// Move '#child' from '#parent1' to '#parent2'
+// Make the textbox in '#target' read-only
+// Select the second option in the selectbox by script
+// Make '#target' double size.
